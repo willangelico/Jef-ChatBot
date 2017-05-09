c49dccc5b883e899025277edb657eb72c0567b80
William Angélico <willangelico@gmail.com>
2017-05-09 18:14:13 -0300

    Nice to meet you

--- app/src/js/ini.js
+++ app/src/js/ini.js
@@ -7,20 +7,15 @@
 		// let txt = "textando babel2";
 		// $('#menu-mobile').click( function(){
 				
-		// 	$('nav').style.left = "0px";
+		// 	$('nav').style.left = "0px"; 
 		// });
 		// $('.close-mobile').click( function(){
 		// 	$('nav').fadeOut();
 		// });
-
-		$("#write-input").on('input', function(){
-			var input = $(this).val();
-			if(input != ""){
-				$("#write-send").addClass('active');
-			}else{
-				$("#write-send").removeClass('active');
-			}
+		$(window).scroll(function() {
+			console.log($("header").outerHeight());
 		});
+
 
 		var talk = {"bootConversation":[
 						{ "id": 1, "dep": false, "dialog":"Valeu por aceitar a conversa. Será bem rapidinho... Vamos lá?" },
@@ -41,8 +36,60 @@
 				</div>
 			</div>`;
 
+		var user = (txt) => `
+			<div class="user">
+				<div class="avatar">
+					<img src="http://lorempixel.com/60/60/people/?1" alt="" class="icon img-circle img-responsive">
+				</div>
+				<div class="msg">
+					<p>${txt}</p>
+				</div>
+			</div>`;
+
 		var status = $('.status');
 		var conversationBox = $('.conversation');
+
+
+		$("#write-input").on('input', function(){
+			var input = $(this).val();
+			if(input != ""){
+				$("#write-send").addClass('active');
+			}else{
+				$("#write-send").removeClass('active');
+			}
+		});
+
+		$("#write-input").bind("enterKey",function(e){
+			$("#write-send").click();
+		});
+		$("#write-input").keyup(function(e){
+		    if(e.keyCode == 13)
+		    {
+		        $(this).trigger("enterKey");
+		    }
+		});
+
+		$("#write-send").on('click',function(){
+			var input = $("#write-input");
+			// console.log(input.attr('data-id'));
+			// console.log(input.val());
+			var next = talk.bootConversation[input.attr('data-id')];
+			console.log(next);
+
+			if(next["condition"] == 'notEmpty'){
+				if(!input.val()){
+					conversationBox.append(user("Você precisa inserir um valor"));	
+				}
+				conversationBox.append(user(input.val()));
+				input.val() == "";
+				var regex = /\$1/;
+
+				next.dialog = next.dialog.replace(regex, input.val());
+				conversationBox.append(robot(next.dialog));
+				bootTalk(input.attr('data-id')+1);
+			}
+			
+		});
 
 		function displayDialog(v,i){
 			let timer = (parseInt(i)+1)*3000;
@@ -58,7 +105,11 @@
 		function displayInput(v,i){
 			//console.log(v+"-"+i)
 			setTimeout(function(){
-				console.log("inout");
+				$('#write-input').prop('disabled', false);
+				$('#write-input').focus();
+				$('#write-input').attr('data-id',i);			
+
+
 			},i*3000);
 			//console.log(i);
 			//bootTalk(i);
@@ -82,10 +133,11 @@
 			// 
 			
 			for (var i in talk.bootConversation){
-				console.log(i+">="+$index);
+				//console.log(i+">="+$index);
 				if(i >= $index){
 					//console.log(talk.bootConversation[i]);
 					//console.log(i);
+					$('#write-input').prop('disabled', true);
 					var v = talk.bootConversation[i];
 					if(v.dep){
 						displayInput(v,i);
